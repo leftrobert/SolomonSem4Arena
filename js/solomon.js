c964a95eaa8ec082bbfa8d712b1fa42628208d30
Nguyen Phuoc Vinh Phu <34395424+leftrobert@users.noreply.github.com>
2021-05-04 02:49:08 +0700

    Trang đăng nhập/đăng ký

--- js/solomon.js
+++ js/solomon.js
@@ -17,3 +17,42 @@ function navTransform() {
         }
     }
 }
+
+function tabSwitch(evt, tabId) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("panel-tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("panel-tab");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" tab-active", "");
+    }
+    document.getElementById(tabId).style.display = "block";
+
+    evt.currentTarget.className += " tab-active";
+}
+
+function labelMove(inputId) {
+    let elm = document.getElementById("l-"+inputId);
+    let ipt = document.getElementById(inputId);
+    if (elm.classList.contains("label-up")) {
+        if (ipt.value === "") elm.classList.remove("label-up");
+    }
+    else elm.classList.add("label-up")
+    if (inputId==="birth") ipt.removeAttribute("onfocus");
+}
+
+function passwordReadability(elm, pwdid) {
+    let pwd = document.getElementById(pwdid);
+    if (elm.classList.contains("icon-show-small")) {
+        pwd.type = "text";
+        elm.classList.remove("icon-show-small");
+        elm.classList.add("icon-hide-small");
+    }
+    else {
+        pwd.type = "password";
+        elm.classList.remove("icon-hide-small");
+        elm.classList.add("icon-show-small");
+    }
+}
